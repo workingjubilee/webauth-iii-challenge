7f3099ca1aa3b5026dd872f1cd8fbd5f70015964
workingjubilee <workingjubilee@gmail.com>
2019-04-03 16:39:26 -0700

    Add curry to curry, also bcrypt, register

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,57 @@
-const Users = require('knex')(require('../knexfile.js').development);
-
+const Users = require('knex')(require('../knexfile.js').development)('users');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
+const generateToken = user => {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, secret, options);  
+}
+
+
+router.post('/register', async (req,res) => {
+  let user = req.body;
+  const oldPass = user.password;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  console.log(user.password);
+
+  try{
+    const success = await Users.insert(user);
+
+    res.status(200).json({ success })
+  } catch(error) {
+    res.status(500).json({
+      message: "What did your crazy code break now?"
+    })
+  }
+
+})
+
+
+router.post('/login', async (req, res) => {
+
+})
 
 router.get('/users', async (req,res) => {
-  res.status(200).send("Yes, it works!")
+  // const token = req.headers.authorization;
+
+  try {
+    let users = await Users.select('*');
+
+    res.status(200).json(users)
+  } catch(error) {
+    res.status(500).json({
+      message: "What did your crazy code break now?"
+    })
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const api = require('./api/auth.js');
+const helmet = require('helmet');
+const cors = require('cors');
+const env = require('dotenv').config();
 
+const api = require('./api/auth.js');
 
 const server = express();
 
-
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 server.use('/api', api)
 
-
-server.listen(4000, () => {
-  console.log("\n\tEVERYONE LOVES CURRY!");
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`\n\tEVERYONE LOVES CURRY! at ${port}`);
 })
